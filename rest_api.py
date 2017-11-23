622ded8597b2f1b2d0303de945404c92b275b074
Tom Matthews <tomtastic@users.noreply.github.com>
2017-11-23 11:38:33 +0000

    remove spurious newlines from results

--- rest_api.py
+++ rest_api.py
@@ -1,9 +1,13 @@
 # Example API Endpoints for use with rest.py
 # 20171101 - TRCM - Moved definitions from main 'rest.py' script to separate module
 #                 - Use logging to spew debug if requested
+# 20171101 - TRCM - Moved definitions from main 'rest.py' script to separate module
+# 20171123 - TRCM - rstrip results text to reomve spurious carriage returns,
+#                 - lowercase the servernames, and re.sub newlines and commas from desc field.
 import time
 import getopt
 import urllib2
+import re
 from lxml import etree as et
 from StringIO import StringIO
 import logging
@@ -11,11 +15,6 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 #logging.basicConfig(filename='rest_api.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 　
 # Our public definitions ###################################
-# !!! These are TEST API Endpoints !!!
-# !!! These are TEST API Endpoints !!!
-# !!! These are TEST API Endpoints !!!
-# Replace api_site and api_url with proper PRODUCTION values
-　
 api_site = 'my_api_server.com'
 　
 def inqCI(appid,ciname,ciclass,flags):
@@ -101,20 +100,20 @@ def CIInquiryResponse(xml):
     cidetails = etree.findall(".//{{{0}}}CIInquiryResponseDetail".format(namespace))
     for ci in cidetails:
         ciname_tag        = ci.find("{{{0}}}CIName".format(namespace))
-        results['CIName'] = ciname_tag.text if ciname_tag is not None else ""
-        results['UUID'] = ciname_tag.attrib['UUID'] if ciname_tag is not None else ""
+        results['CIName'] = ciname_tag.text.rstrip().lower() if ciname_tag is not None else ""
+        results['UUID'] = ciname_tag.attrib['UUID'].rstrip() if ciname_tag is not None else ""
         ciclass_tag       = ci.find("{{{0}}}CIClass".format(namespace))
-        results['CIClass'] = ciclass_tag.text if ciclass_tag is not None else ""
+        results['CIClass'] = ciclass_tag.text.rstrip() if ciclass_tag is not None else ""
         status_tag        = ci.find("{{{0}}}Status".format(namespace))
-        results['Status'] = status_tag.text if status_tag is not None else ""
+        results['Status'] = status_tag.text.rstrip() if status_tag is not None else ""
         active_tag        = ci.find("{{{0}}}Active".format(namespace))
-        results['Active'] = active_tag.text if active_tag is not None else ""
+        results['Active'] = active_tag.text.rstrip() if active_tag is not None else ""
         groupown_tag        = ci.find("{{{0}}}OwningGroup".format(namespace))
-        results['OwningGroup'] = groupown_tag.text if groupown_tag is not None else ""
+        results['OwningGroup'] = groupown_tag.text.rstrip() if groupown_tag is not None else ""
         groupsup_tag        = ci.find("{{{0}}}SupportingGroup".format(namespace))
-        results['SupportingGroup'] = groupsup_tag.text if groupsup_tag is not None else ""
+        results['SupportingGroup'] = groupsup_tag.text.rstrip() if groupsup_tag is not None else ""
         purpose_tag       = ci.find("{{{0}}}Purpose".format(namespace))
-        results['Purpose'] = purpose_tag.text if purpose_tag is not None else ""
+        results['Purpose'] = purpose_tag.text.rstrip() if purpose_tag is not None else ""
     return results
 　
 def CIListResponse(xml):
@@ -136,18 +135,19 @@ def CIListResponse(xml):
     for ci in cidetails:
         results[ci] = {}  # Why is there auto-vivification in Python?
         ciname_tag        = ci.find("{{{0}}}CIName".format(namespace))
-        results[ci]['CIName'] = ciname_tag.text if ciname_tag is not None else ""
-        results[ci]['UUID'] = ciname_tag.attrib['UUID'] if ciname_tag is not None else ""
+        results[ci]['CIName'] = ciname_tag.text.rstrip().lower() if ciname_tag is not None else ""
+        results[ci]['UUID'] = ciname_tag.attrib['UUID'].rstrip() if ciname_tag is not None else ""
         ciclass_tag       = ci.find("{{{0}}}CIClass".format(namespace))
-        results[ci]['CIClass'] = ciclass_tag.text if ciclass_tag is not None else ""
+        results[ci]['CIClass'] = ciclass_tag.text.rstrip() if ciclass_tag is not None else ""
         status_tag        = ci.find("{{{0}}}Status".format(namespace))
-        results[ci]['Status'] = status_tag.text if status_tag is not None else ""
+        results[ci]['Status'] = status_tag.text.rstrip() if status_tag is not None else ""
         active_tag        = ci.find("{{{0}}}Active".format(namespace))
-        results[ci]['Active'] = active_tag.text if active_tag is not None else ""
+        results[ci]['Active'] = active_tag.text.rstrip() if active_tag is not None else ""
         cidescription_tag = ci.find("{{{0}}}CIDescription".format(namespace))
-        results[ci]['CIDescription'] = cidescription_tag.text if cidescription_tag is not None else ""
+        results[ci]['CIDescription'] = cidescription_tag.text.rstrip() if cidescription_tag is not None else ""
+        results[ci]['CIDescription'] = re.sub('[\n,]+',' ',results[ci]['CIDescription'])
         purpose_tag       = ci.find("{{{0}}}Purpose".format(namespace))
-        results[ci]['Purpose'] = purpose_tag.text if purpose_tag is not None else ""
+        results[ci]['Purpose'] = purpose_tag.text.rstrip() if purpose_tag is not None else ""
     return results
 　
 def CIListRequest(appid_param,ciname_param,ciclass_param):
@@ -177,30 +177,3 @@ def CIListRequest(appid_param,ciname_param,ciclass_param):
     xml = et.tostring(cilist_tree, xml_declaration=True, encoding='UTF-8')
     return xml
 　
-def CIInquiryRequest(appid_param,ciname_param,ciclass_param):
-    XML_NS        = "http://my_api_server.com/rest/CIInquiryRequest_v1.0.xsd"
-    NS_MAP        = {None: XML_NS} # The default namespace
-    api_type      = 'CIInquiryRequest'
-    action_attr   = 'InquiryRequest'
-    function_attr = 'CIInquiry'
-    epoch_attr    = str(int(time.time()))
-    version_attr  = '1.0'
-    # Create an ElementTree instance
-    query = et.Element(api_type, nsmap=NS_MAP,
-                    action=action_attr,
-                    function=function_attr,
-                    genDateTZ=epoch_attr,
-                    version=version_attr)
-    ciinquiry_tree = et.ElementTree(query)
-    appid = et.SubElement(query, 'ApplicationID')
-    appid.text = appid_param
-    detail = et.SubElement(query, 'CIInquiryRequestDetail')
-    ciname = et.SubElement(detail, 'CIName')
-    ciname.text = ciname_param
-    ciclass = et.SubElement(detail, 'CIClass')
-    ciclass.text = ciclass_param
-    # At this point, lets return the stringified element tree
-    #xml = et.tostring(ciinquiry_tree, xml_declaration=True, pretty_print=True, encoding='UTF-8')
-    xml = et.tostring(ciinquiry_tree, xml_declaration=True, encoding='UTF-8')
-    return xml
-    
